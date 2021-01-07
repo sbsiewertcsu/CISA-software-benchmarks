6a20345e059967fe69fc00e667ad20311e04a630
sbsiewertcsu <sbsiewert@csuchico.edu>
2021-01-07 11:43:04 -0700

    Removed gratituous include in pyrimage.cpp for imgcodecs - not needed.

--- power-efficiency-benchmarks/pyrimage.cpp
+++ power-efficiency-benchmarks/pyrimage.cpp
@@ -1,10 +1,10 @@
 #include "iostream"
 
-//#include "opencv2/imgproc.hpp"
 //#include "opencv2/imgcodecs.hpp"
+//#include "opencv2/imgcodecs/imgcodecs.hpp"
 
+//#include "opencv2/imgproc.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/imgcodecs/imgcodecs.hpp"
 
 #include "opencv2/highgui.hpp"
 #include <time.h>
